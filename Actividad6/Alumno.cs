7eebf34a0a4844ca667cc11481f8e5e026d22059
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-04-08 13:38:15 +0200

    Agregar archivos de proyecto.

---
+++ Actividad6/Alumno.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Actividad6
+{
+    public class Alumno
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string Nombre { get; set; }
+        public int Nota { get; set; }
+
+        public static List<string> getNombres(List<Alumno> alumnos)
+        {
+            return alumnos.Select(a => a.Nombre).ToList();
+        }
+
+        public static Alumno getListadoNota(List<Alumno> alumnos, int nota)
+        {
+            return alumnos.FirstOrDefault(a => a.Nota == nota);
+        }
+
+        public static Alumno getPrimerAlumno(List<Alumno> alumnos)
+        {
+            return alumnos.FirstOrDefault();
+        }
+
+        public static Alumno getUltimoAlumno(List<Alumno> alumnos)
+        {
+            return alumnos.LastOrDefault();
+        }
+
+        public static int getSumaNota(List<Alumno> alumnos)
+        {
+            return alumnos.Sum(a => a.Nota);
+        }
+
+        public static int getNotaMaxima(List<Alumno> alumnos)
+        {
+            return alumnos.Max(a => a.Nota);
+        }
+
+        public static int getNotaMinima(List<Alumno> alumnos)
+        {
+            return alumnos.Min(a => a.Nota);
+        }
+
+        public static int getNotaPorNombre(List<Alumno> alumnos, string v)
+        {
+            return alumnos.FirstOrDefault(a => a.Nombre.Equals(v)).Nota;
+        }
+
+        public static double getNotaMedia(List<Alumno> alumnos)
+        {
+            return Math.Round(alumnos.Average(a => a.Nota), 2);
+        }
+    }
+}
